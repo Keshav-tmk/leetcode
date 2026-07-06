95d70939621e0a7b7701d50231da38647c1a56a0
Keshav N <keshav.tmk06@gmail.com>
2026-07-06 19:56:02 +0530

    Time: 9 ms (4.64%) | Memory: 46.1 MB (10.47%) - LeetSync

--- Recursion/39-combination-sum/combination-sum.java
+++ Recursion/39-combination-sum/combination-sum.java
@@ -1,18 +1,20 @@
-class Solution {
-    public static void comb(int i, int[] arr, List<List<Integer>> temp, List<Integer> ans, int target) {
-        if (i == arr.length) {
-            if (target == 0) {
-
+class Solution {Set<List<Integer>> set=new HashSet<>();
+    public  void comb(int i, int[] arr, List<List<Integer>> temp, List<Integer> ans, int target) {
+        
+        if (i == arr.length || target < 0)
+            return;
+        if (target == 0) {
+            if(!set.contains(ans)){
                 temp.add(new ArrayList<>(ans));
-
+                set.add(ans);
             }
             return;
         }
-        if (arr[i] <= target) {
-            ans.add(arr[i]);
-            comb(i, arr, temp, ans, target - arr[i]);
-            ans.remove(ans.size() - 1);
-        }
+
+        ans.add(arr[i]);
+        comb(i + 1, arr, temp, ans, target - arr[i]);
+        comb(i, arr, temp, ans, target - arr[i]);
+        ans.remove(ans.size() - 1);
         comb(i + 1, arr, temp, ans, target);
 
     }
