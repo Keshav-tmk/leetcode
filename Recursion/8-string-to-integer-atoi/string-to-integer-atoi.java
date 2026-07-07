5a98d82011717276f1d3d5b6eea13a24a25354a1
Keshav N <keshav.tmk06@gmail.com>
2026-07-07 20:05:03 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (6.96%) - LeetSync

---
+++ Recursion/8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int myAtoi(String t) {
+        String s = t.trim();
+        if (s.isEmpty())
+            return 0;
+        int i = 0, si = 1;
+        long res = 0;
+        if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+            si = s.charAt(i) == '-' ? -1 : 1;
+            i++;
+        }
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            res = res * 10 + s.charAt(i) - '0';
+
+            if (res * si > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            if (res * si < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+            i++;
+        }
+        return (int)(si * res);
+
+    }
+}
