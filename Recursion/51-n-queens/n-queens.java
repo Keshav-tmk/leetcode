7a7e24e4d71bcaadd4e0bf24a5c6b3e5698559df
Keshav N <keshav.tmk06@gmail.com>
2026-07-09 06:57:46 +0530

    Time: 2 ms (83.47%) | Memory: 46.7 MB (46.73%) - LeetSync

--- Recursion/51-n-queens/n-queens.java
+++ Recursion/51-n-queens/n-queens.java
@@ -1,60 +1,54 @@
 class Solution {
-    List<List<String>> ans=new ArrayList<>();
-    public boolean isSafe(char[][] board,int row,int col,int n){
+    List<List<String>>ans=new ArrayList<>();
+
+    public boolean isSafe(char[][] b,int r,int c,int n){
+
         for(int i=0;i<n;i++){
-            if(board[row][i]=='Q')
-                return false;
+            if(b[i][c]=='Q')return false;
 
         }
-        for(int i=0;i<n;i++){
-            if(board[i][col]=='Q')
-                return false;
-                
-        }
-
-        for(int i=row-1, j=col-1;i>=0 && j>=0;i--,j--){
-            if(board[i][j]=='Q')return false;
+        for(int i=r-1,j=c-1;i>=0&&j>=0;i--,j--){
+            if(b[i][j]=='Q')return false;
 
         }
-        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
-            if(board[i][j]=='Q')return false;
+        for(int i=r-1,j=c+1;i>=0&&j<n;i--,j++){
+            if(b[i][j]=='Q')return false;
             
-        }
-        return true;
+        }return true;
     }
 
-    public void help(char[][] board,int row,int n){
+    public void help(char b[][],int row,int n){
         if(row==n){
-                List<String> temp=new ArrayList<>();
-                for(char[] ch:board){
-                    temp.add(new String(ch));
-                }
+            List<String> temp=new ArrayList<>();
+            for(char[] r: b){
+                temp.add(new String(r));
 
-                ans.add(temp);
-                return;
-
+            }
+            ans.add(temp);
+            return;
         }
-
         for(int j=0;j<n;j++){
-            if(isSafe(board,row,j,n)){
-                board[row][j]='Q';
-                help(board,row+1,n);
-                board[row][j]='.';
-                           }
+        if(isSafe(b,row,j,n)){
+            b[row][j]='Q';
+            help(b,row+1,n);
+            b[row][j]='.';
         }
+    }
+
     }
 
     public List<List<String>> solveNQueens(int n) {
-        char[][] board=new char [n][n];
+        char [][] b=new char[n][n];
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                board[i][j]='.';
+                b[i][j]='.';
             }
         }
-        help(board,0,n);
+        help(b,0,n);
         return ans;
 
+        
         
     }
 }
