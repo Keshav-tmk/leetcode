4ad4f8e97a4f01e2a72622d8ec7a7a41f82e0468
Keshav N <keshav.tmk06@gmail.com>
2026-07-14 12:25:20 +0530

    Time: 10 ms (84.34%) | Memory: 50.3 MB (37.66%) - LeetSync

--- Binary_Search/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ Binary_Search/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,42 +1,37 @@
 class Solution {
+    public boolean help(int[] arr,int days,int cap){
+        int sum=0,cnt=1;
+        for(int i:arr){
+            if(i>cap)return false;
+            
+            if(sum+i>cap){
+                cnt++;
+                sum=i;
+            }
+            else{
+                sum+=i;
+            }
+            
+        }
+        if(cnt<=days)return true;
+        return false;
+    }
     public int shipWithinDays(int[] weights, int days) {
-
-        int max=0,sum=0;
+        int sum=0;
         for(int i:weights){
-            max=Math.max(max,i);
             sum+=i;
         }
-        int low=max,high=sum,ans=0;
+        int l=1,r=sum,ans=0;
 
-        // for(int i=low;i<=high;i++){
-
-        //     if(ship(weights,days,i))return i;
-        // }
-        // return sum;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(ship(weights,days,mid)){
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(help(weights,days,mid)){
                 ans=mid;
-                high=mid-1;
+                r=mid-1;
             }
-            else low=mid+1;
+            else l=mid+1;
         }
         return ans;
-        
-    }
-    public boolean ship(int[] arr,int days,int cap){
-        int d=1,sum=0;
-        for(int w:arr){
-            sum+=w;
-            if(sum>cap){
-                d++;
-                sum=w;
-            }
-
-            
-
-        }if(d<=days)return true;
-        return false;
-
     }
 }
