0c50bcb03febcb9a94f7d08fed57b0a67854a2ae
Keshav N <keshav.tmk06@gmail.com>
2026-07-22 14:56:41 +0530

    Time: 1 ms (33.00%) | Memory: 46.3 MB (75.32%) - LeetSync

--- Trees/25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ Trees/25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -11,36 +11,78 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
 
-        List<ListNode> ans = new ArrayList<>();
-        ListNode temp = head;
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        dummy.next = head;
-        while (temp != null) {
-            ans.add(temp);
-            temp = temp.next;
+        ListNode dummy=new ListNode(0);
 
-        }
-        for (int i = 0; i + k <= ans.size(); i = i + k) {
-            int l = i;
-            int r = i + k - 1;
-            while (l < r) {
-                ListNode t = ans.get(l);
-                ans.set(l, ans.get(r));
-                ans.set(r, t);
-                l++;
-                r--;
+        dummy.next=head;
+        ListNode prevgrp=dummy;
+        
+
+        while(true){
+            ListNode kth=prevgrp;
+
+            for(int i=0;i<k&& kth!=null;i++){
+                kth=kth.next;
             }
-        }
-        temp = head;
-        for (ListNode i : ans) {
-            curr.next = i;
-            curr = curr.next;
-        }
+            if(kth==null)break;
+            ListNode grpnext=kth.next;
+            ListNode prev=grpnext;
+            ListNode curr=prevgrp.next;
+            while(curr!=grpnext){
+                ListNode next=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
+            }
+            ListNode temp=prevgrp.next;
+            prevgrp.next=prev;
+            temp.next=curr;
+            prevgrp=temp;
 
-        curr.next = null;
+            
+        }
 
         return dummy.next;
+
+
+
+
+
+
+
+
+
+
+
+        // List<ListNode> ans = new ArrayList<>();
+        // ListNode temp = head;
+        // ListNode dummy = new ListNode(0);
+        // ListNode curr = dummy;
+        // dummy.next = head;
+        // while (temp != null) {
+        //     ans.add(temp);
+        //     temp = temp.next;
+
+        // }
+        // for (int i = 0; i + k <= ans.size(); i = i + k) {
+        //     int l = i;
+        //     int r = i + k - 1;
+        //     while (l < r) {
+        //         ListNode t = ans.get(l);
+        //         ans.set(l, ans.get(r));
+        //         ans.set(r, t);
+        //         l++;
+        //         r--;
+        //     }
+        // }
+        // temp = head;
+        // for (ListNode i : ans) {
+        //     curr.next = i;
+        //     curr = curr.next;
+        // }
+
+        // curr.next = null;
+
+        // return dummy.next;
 
     }
 }
