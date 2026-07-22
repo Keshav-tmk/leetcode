50cec2988a5fbc65ac04a380bac29ddc6858b66e
Keshav N <keshav.tmk06@gmail.com>
2026-07-22 19:50:42 +0530

    Time: 1 ms (99.92%) | Memory: 49.1 MB (71.91%) - LeetSync

--- Trees/143-reorder-list/reorder-list.java
+++ Trees/143-reorder-list/reorder-list.java
@@ -10,27 +10,63 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        List<Integer> ans=new ArrayList<>();
-        
-        ListNode temp=head;
-        while(temp!=null){
-            ans.add(temp.val);
-            temp=temp.next;
-        }
-        temp=head;
-        int l=0,r=ans.size()-1;
-        while(l<r){
-             temp.val=ans.get(l);
-             temp=temp.next;
-             temp.val=ans.get(r);
-             temp=temp.next;
-             l++;r--;
-        }
-        if (l == r) {
-    temp.val = ans.get(l);
-}
+        ListNode slow=head,fast=head;
+    while(fast.next!=null && fast.next.next!=null){
+        slow=slow .next;
+        fast=fast.next.next;
+    }
+    ListNode temp=head;
+    ListNode mid=slow;
+    
+    ListNode curr=mid.next;
+    mid.next=null;
+    ListNode prev=null;
+    while(curr!=null){
+        ListNode next=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=next;
+    }
 
-       return ; 
+    ListNode head2=prev;
+
+    while(head2!=null){
+        ListNode next1=temp.next;
+        temp.next=head2;
+        ListNode next2=head2.next;
+        head2.next=next1;
+
+        temp=next1;
+        head2=next2;
+    }
+
+
+
+
+
+
+
+
+//         List<Integer> ans=new ArrayList<>();
         
+//         ListNode temp=head;
+//         while(temp!=null){
+//             ans.add(temp.val);
+//             temp=temp.next;
+//         }
+//         temp=head;
+//         int l=0,r=ans.size()-1;
+//         while(l<r){
+//              temp.val=ans.get(l);
+//              temp=temp.next;
+//              temp.val=ans.get(r);
+//              temp=temp.next;
+//              l++;r--;
+//         }
+//         if (l == r) {
+//     temp.val = ans.get(l);
+// }
+
+//        return ; 
     }
 }
