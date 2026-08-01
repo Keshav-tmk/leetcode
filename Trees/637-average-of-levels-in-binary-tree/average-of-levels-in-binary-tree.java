da8feb453d2da2412d1edda838eb129de6038db7
Keshav N <keshav.tmk06@gmail.com>
2026-08-01 20:16:56 +0530

    Time: 2 ms (97.49%) | Memory: 48.7 MB (28.27%) - LeetSync

--- Trees/637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ Trees/637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -14,28 +14,31 @@
  * }
  */
 class Solution {
-    public List<Double> averageOfLevels(TreeNode root) {
+    
 
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans=new ArrayList<>();
+        if(root==null)return ans;
+        
         Queue<TreeNode> q=new LinkedList<>();
-        List<Double> list=new ArrayList<>();
         q.offer(root);
+
         while(!q.isEmpty()){
             int n=q.size();
             double sum=0;
-           
             for(int i=0;i<n;i++){
-                 TreeNode curr=q.poll();
-                 sum+=curr.val;
-                 if(curr.left!=null)q.offer(curr.left);
-                 if(curr.right!=null)q.offer(curr.right);
-
-
-                 
+                TreeNode curr=q.poll();
+                sum+=curr.val;
+                if(curr.left!=null)q.offer(curr.left);
+                if(curr.right!=null)q.offer(curr.right);
             }
-                // double avg=sum/n
-            list.add(sum/n);
+            ans.add(sum/n);
         }
-        return list;
+        
+
+        
+        return ans;
+        
         
     }
 }
