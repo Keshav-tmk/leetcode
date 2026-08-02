e464c464bc2aeb639f923ccd287b9feaf2654e37
Keshav N <keshav.tmk06@gmail.com>
2026-08-02 13:25:58 +0530

    Time: 19 ms (12.64%) | Memory: 74.5 MB (89.20%) - LeetSync

---
+++ Trees/1731-even-odd-tree/even-odd-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isvalid(List<Integer> list,boolean even){
+        if(even==true){
+            for(int i=1;i<list.size();i++){
+                if(list.get(i)<=list.get(i-1))return false;
+            }
+            
+        }
+        else{
+            for(int i=1;i<list.size();i++){
+                if(list.get(i)>=list.get(i-1))return false;
+            }
+
+        }
+        return true;
+    }
+    public boolean isEvenOddTree(TreeNode root) {
+        
+        if(root==null)return true;
+        Queue<TreeNode> q=new LinkedList<>();
+        
+        q.offer(root);
+        boolean even=true;
+
+        while(!q.isEmpty()){
+            int n=q.size();
+            List<Integer> list=new ArrayList<>();
+           
+
+            for(int i=0;i<n;i++){
+                TreeNode curr=q.poll();
+                if(even==true){
+                    if(curr.val %2==0)return false;
+                }
+                if(even ==false){
+                    if(curr.val%2==1)return false;
+                }
+                list.add(curr.val);
+                if(curr.left!=null)q.offer(curr.left);
+                if(curr.right!=null)q.offer(curr.right);
+            }
+            if(!isvalid(list,even))return false;
+            even=!even;
+        }
+    return true;
+    }
+}
