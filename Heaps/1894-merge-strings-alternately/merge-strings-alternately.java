96bca4bddde593b52b73215a343b566ccad683ef
Keshav N <keshav.tmk06@gmail.com>
2026-08-05 23:23:57 +0530

    Time: 1 ms (96.05%) | Memory: 43.1 MB (63.70%) - LeetSync

---
+++ Heaps/1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String mergeAlternately(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        int i=0,j=0;
+        StringBuilder sb=new StringBuilder();
+        while(i<n&&j<m){
+            sb.append(s.charAt(i++));
+            sb.append(t.charAt(j++));
+
+        }
+        while(i<n){
+            sb.append(s.charAt(i++));
+           
+
+        }
+        while(j<m){
+          
+            sb.append(t.charAt(j++));
+
+        }
+
+        return sb.toString();
+        
+    }
+}
