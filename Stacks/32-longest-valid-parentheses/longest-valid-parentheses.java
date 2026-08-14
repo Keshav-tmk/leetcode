a8d1805000c73baefe89f8f47cf5b26e44b80450
Keshav N <keshav.tmk06@gmail.com>
2026-08-14 13:28:29 +0530

    Time: 2 ms (97.06%) | Memory: 44.9 MB (90.26%) - LeetSync

--- Stacks/32-longest-valid-parentheses/longest-valid-parentheses.java
+++ Stacks/32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,28 +1,58 @@
+// class Solution {
+//     public int longestValidParentheses(String s) {
+//         if (s.isEmpty())
+//             return 0;
+//         Stack<Integer> st = new Stack<>();
+//         st.push(-1);
+//         int max=0;
+
+//         for (int i=0;i<s.length();i++) {
+//             char ch=s.charAt(i);
+//             if (ch == '(') {
+//                 st.push(i);
+
+//             } else {
+//                 st.pop();
+
+//                 if (st.isEmpty()) {
+//                     st.push(i);
+//                 } else {
+//                     max = Math.max(max, i - st.peek());
+//                 }
+
+//             }
+//         }
+//         return max;
+
+//     }
+// }
+
 class Solution {
     public int longestValidParentheses(String s) {
-        if (s.isEmpty())
-            return 0;
-        Stack<Integer> st = new Stack<>();
-        st.push(-1);
+        int l=0,r=0;
         int max=0;
 
-        for (int i=0;i<s.length();i++) {
-            char ch=s.charAt(i);
-            if (ch == '(') {
-                st.push(i);
+        for(char ch:s.toCharArray()){
+            if(ch=='(')l++;
+            else r++;
 
-            } else {
-                st.pop();
-
-                if (st.isEmpty()) {
-                    st.push(i);
-                } else {
-                    max = Math.max(max, i - st.peek());
-                }
-
+            if(l==r)max=Math.max(max,2*r);
+            if(r>l){
+                l=0;r=0;
             }
         }
-        return max;
+        l=0;r=0;
+
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(ch=='(')l++;
+            else r++;
+            if(l==r)max=Math.max(max,2*r);
+            else if(l>r){
+                l=0;r=0;
+            }
+        }
+       return max;
 
     }
 }
